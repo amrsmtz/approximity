f3d6874c21e90e59e0c2986970d6f03711b2ad6d
Jeremy Meimouni <jeremy.meimouni@gmail.com>
2018-11-19 17:59:48 -0500

    adding ltn and lat to business

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_214616) do
+ActiveRecord::Schema.define(version: 2018_11_19_225928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2018_11_19_214616) do
     t.text "reviews"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "journey_businesses", force: :cascade do |t|
