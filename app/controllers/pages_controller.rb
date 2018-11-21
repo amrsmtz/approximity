c6c3e83c4a808218bbd64119901dffb403422703
Jeremy Meimouni <jeremy.meimouni@gmail.com>
2018-11-21 15:10:55 -0500

    search bar function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def home
-    @businesses = Business.all
+    if params[:query].present?
+      @businesses = Business.search_by_name_and_category(params[:query])
+    else
+      @businesses = Business.all
+    end
   end
 end
