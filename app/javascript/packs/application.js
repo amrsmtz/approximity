e2f6d5a9ea52a5e126e750fb5683fe03ab9cb4b7
Alexandre Schmitz <alexandre.schmitz@cerbo.me>
2018-11-20 20:26:27 -0500

    typed on landing plus some front

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["BAKERY", "SHOE REPAIR", "BUTCHER"],
+  typeSpeed: 40,
+  loop: true
+}
+
+var typed = "Look for: " + new Typed("#typedjs", options);
