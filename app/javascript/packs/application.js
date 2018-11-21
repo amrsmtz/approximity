b64e497bc3286da4ffa652db46bb482ad10044b3
John Freeman <Johngrahamfreeman@gmail.com>
2018-11-21 11:56:42 -0500

    Hid my Map API key

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,16 +11,17 @@ var typed = "Look for: " + new Typed("#typedjs", options);
 
 const map_button = document.querySelector("#map-button");
 
-map_button.addEventListener("click", (event) => {
-  if (document.querySelector(".mapboxgl-canary") === null) {
-    setTimeout(() => {
-      mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obmdyYWhhbWZyZWVtYW4iLCJhIjoiY2pvcHo4bTRzMDh4OTNxcXZsMW9weG5sMCJ9.VqybDrZRkykOvm1vN8wGhw';
-      var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9'
-      });
-    }, 1000);
-  }
-})
-
+if (map_button) {
+  map_button.addEventListener("click", (event) => {
+    if (document.querySelector(".mapboxgl-canary") === null) {
+      setTimeout(() => {
+        mapboxgl.accessToken = process.env.publickey;
+        var map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v9'
+        });
+      }, 1000);
+    }
+  })
+}
 
