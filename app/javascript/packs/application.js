7fe8237295c96db255c40ae83c4a2d3ef63e7537
Alexandre Schmitz <alexandre.schmitz@cerbo.me>
2018-11-22 17:19:17 -0500

    included the directions in the map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,11 @@
 import "bootstrap";
 import Typed from 'typed.js';
+import 'mapbox-gl/dist/mapbox-gl.css'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+
+
 
 var options = {
   strings: ["BAKERY", "SHOE REPAIR", "BUTCHER"],
@@ -9,6 +15,33 @@ var options = {
 
 if (document.getElementById('typedjs')) {
   var typed = "Look for: " + new Typed("#typedjs", options);
+}
+
+// Mapbox Map page
+
+const mapElement = document.getElementById('map');
+
+if (mapElement) { // only build a map if there's a div#map to inject into
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
+  const directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+    unit: 'metric',
+    profile: 'mapbox/walking',
+    controls: {
+      inputs: true
+    }
+  });
+
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+  });
+
+  directions.setOrigin([-73.567256, 45.5016889]);
+  directions.setDestination([-73.6, 45.5016889]);
+
+  map.addControl(directions, 'top-left');
 }
 
 // Autocomplete on the start location and end location
